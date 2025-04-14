a9df50539559670aeef5e1d54fe4dffd60e7e6eb
Girraj <girrajsingh7898@gmail.com>
2025-04-14 09:25:46 +0530

    some responsive design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,25 @@
 
-        const swiper = new Swiper('.hover_slider', {
-            direction: 'vertical',
-            mousewheel: true,
-            keyboard: {
-                enabled: true,
-                onlyInViewport: false
-            },
-            effect: 'coverflow',
-            coverflowEffect: {
-                rotate: 50,
-                stretch: 0,
-                depth: 100,
-                modifier: 1,
-                slideShadows: true,
-            },
-            speed: 1000,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-        });
+        // const swiper = new Swiper('.hover_slider', {
+        //     direction: 'vertical',
+        //     mousewheel: true,
+        //     keyboard: {
+        //         enabled: true,
+        //         onlyInViewport: false
+        //     },
+        //     effect: 'coverflow',
+        //     coverflowEffect: {
+        //         rotate: 50,
+        //         stretch: 0,
+        //         depth: 100,
+        //         modifier: 1,
+        //         slideShadows: true,
+        //     },
+        //     speed: 1000,
+        //     pagination: {
+        //         el: '.swiper-pagination',
+        //         clickable: true,
+        //     },
+        // });
 
         const items = document.querySelectorAll('.hover_slider_image');
         items.forEach(item => {
@@ -35,3 +35,27 @@
                 item.style.transform = 'perspective(1000px) rotateY(0) rotateX(0) scale3d(1, 1, 1)';
             });
         });
+
+
+        // const swiperEl = document.querySelector('.product_slider_1')
+        // Object.assign(swiperEl, {
+          
+        //   pagination: {
+        //     clickable: true,
+        //   },
+        //   breakpoints: {
+        //     640: {
+        //       slidesPerView: 2,
+        //       spaceBetween: 20,
+        //     },
+        //     768: {
+        //       slidesPerView: 4,
+        //       spaceBetween: 40,
+        //     },
+        //     1024: {
+        //       slidesPerView: 5,
+        //       spaceBetween: 50,
+        //     },
+        //   },
+        // });
+        // swiperEl.initialize();
